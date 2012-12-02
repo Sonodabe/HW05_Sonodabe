442a79e5de242e1a9166a8185db0d365980e2de5
Brandon Sonoda <sonodabe@muohio.edu>
2012-12-02 14:17:47 -0500

    Finished rough initial implementation of GraphMatrix

---
+++ ListGraph.cpp
@@ -0,0 +1,16 @@
+#include "ListGraph.h"
+
+
+ListGraph::ListGraph(int numNodes){
+    
+}
+
+ListGraph::~ListGraph(){}
+
+void ListGraph::addEdge(NodeID u, NodeID v, EdgeWeight weight){}
+
+EdgeWeight ListGraph::weight(NodeID u, NodeID v) const{}
+std::list<NWPair> ListGraph::getAdj(NodeID u) const{}
+unsigned ListGraph::degree(NodeID u) const{}
+unsigned ListGraph::size() const{}
+unsigned ListGraph::numEdges() const{}
--- src/MatrixGraph.cpp
+++ src/MatrixGraph.cpp
@@ -4,30 +4,44 @@
 
 MatrixGraph::MatrixGraph(unsigned num_nodes) {
 	M.resize(num_nodes);
-
+    for(int i = 0; i<num_nodes; i++){
+        M[i].resize(num_nodes);
+        for(int j = 0; j<num_nodes; j++)
+            M[i][j] = 0;
+    }
+    num_edges = 0;
 }
 
 MatrixGraph::~MatrixGraph() {
 }
 
 void MatrixGraph::addEdge(NodeID u, NodeID v, EdgeWeight weight){
-	
+    M[u][v] = M[v][u] = weight;
+    num_edges++;
 }
 
 EdgeWeight MatrixGraph::weight(NodeID u, NodeID v) const {
-	return NULL;
+	return M[u][v];
 }
 
 std::list<NWPair> MatrixGraph::getAdj(NodeID u) const {
-
+    EList* adj = new EList();
+    for(int i = 0; i<M[u].size(); i++)
+        adj->push_back(std::make_pair(u, M[u][i]));
+    
+    return *adj;
 }
 
 unsigned MatrixGraph::degree(NodeID u) const {
-
+    unsigned count = 0;
+    for(int i = 0; i<M[u].size(); i++){
+        if(M[u][i] != 0) count++;
+    }
+    return count;
 }
 
 unsigned MatrixGraph::size() const {
-	return 0;
+	return (unsigned)(M.size());
 }
 
 unsigned MatrixGraph::numEdges() const {
