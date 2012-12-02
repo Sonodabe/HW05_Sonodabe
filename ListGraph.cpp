cb7d1af8380dbec1c49bfe3eddac81b49b922c2f
Brandon Sonoda <sonodabe@muohio.edu>
2012-12-02 14:28:36 -0500

    Finished rough implementation of listGraph
    ...Time for testing part one

--- ListGraph.cpp
+++ ListGraph.cpp
@@ -2,27 +2,36 @@
 
 
 ListGraph::ListGraph(int numNodes){
-    
+    edgeList.resize(numNodes);
+    num_edges = 0;
 }
 
 ListGraph::~ListGraph(){}
 
 void ListGraph::addEdge(NodeID u, NodeID v, EdgeWeight weight){
+    edgeList[u].push_back(std::make_pair(v, weight));
+    edgeList[v].push_back(std::make_pair(u, weight));
+    num_edges++;
 }
 
 EdgeWeight ListGraph::weight(NodeID u, NodeID v) const{
-    return 0.0;
+    EList::const_iterator iter;
+    for(iter = edgeList[u].begin(); iter!=edgeList[u].end(); iter++){
+        NWPair cur = *iter;
+        if(cur.first == v)
+            return cur.second;
+    }
+    return 0;
 }
 std::list<NWPair> ListGraph::getAdj(NodeID u) const{
-    EList* adj = new EList();
-    return *adj;
+    return edgeList[u];
 }
 unsigned ListGraph::degree(NodeID u) const{
-    return 0;
+    return (unsigned)edgeList[u].size();
 }
 unsigned ListGraph::size() const{
-    return 0;
+    return (unsigned)edgeList.size();
 }
 unsigned ListGraph::numEdges() const{
-    return 0;
+    return num_edges;
 }
