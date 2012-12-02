afce98435329e31e2eb0677c6b8137f9c66327ae
Brandon Sonoda <sonodabe@muohio.edu>
2012-12-02 14:19:48 -0500

    Added Stubs for ListGraph

--- ListGraph.cpp
+++ ListGraph.cpp
@@ -7,10 +7,22 @@ ListGraph::ListGraph(int numNodes){
 
 ListGraph::~ListGraph(){}
 
-void ListGraph::addEdge(NodeID u, NodeID v, EdgeWeight weight){}
+void ListGraph::addEdge(NodeID u, NodeID v, EdgeWeight weight){
+}
 
-EdgeWeight ListGraph::weight(NodeID u, NodeID v) const{}
-std::list<NWPair> ListGraph::getAdj(NodeID u) const{}
-unsigned ListGraph::degree(NodeID u) const{}
-unsigned ListGraph::size() const{}
-unsigned ListGraph::numEdges() const{}
+EdgeWeight ListGraph::weight(NodeID u, NodeID v) const{
+    return 0.0;
+}
+std::list<NWPair> ListGraph::getAdj(NodeID u) const{
+    EList* adj = new EList();
+    return *adj;
+}
+unsigned ListGraph::degree(NodeID u) const{
+    return 0;
+}
+unsigned ListGraph::size() const{
+    return 0;
+}
+unsigned ListGraph::numEdges() const{
+    return 0;
+}
