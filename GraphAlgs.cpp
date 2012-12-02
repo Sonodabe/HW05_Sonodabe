3e9491acca92a779ab115713f1e4525380e9ba1a
Brandon Sonoda <sonodabe@muohio.edu>
2012-12-02 15:10:37 -0500

    Lost motivation...
    Added the pseudocoded tour method… break time.

--- GraphAlgs.cpp
+++ GraphAlgs.cpp
@@ -14,3 +14,16 @@ std::pair<std::vector<NodeID>, EdgeWeight> TSP(Graph* G){
     vector<NodeID> s;
     return make_pair(s, 0);
 }
+
+std::pair<std::vector<NodeID>, EdgeWeight> tour(int* arr, int arr_len, int start, EdgeWeight cur,
+                                                std::pair<std::vector<NodeID>, EdgeWeight> best){
+    if(arr_len - start == 1){
+            //Check to see if this is the best of the best of the best tour. <- MIB reference
+    }
+    //Check all other perms
+    for(int i = start; i<arr_len; i++){
+        //Swap arr[start] and ar[i]
+        tour(arr, arr_len, start+1, cur+arr[i], best);
+        //Swap back to maintain order
+    }
+}
