8535581c7de6f862db977f4462cc39a13e83eb91
Brandon Sonoda <sonodabe@muohio.edu>
2012-12-02 20:38:36 -0500

    #NextPermutation
    Nothing like the standard library to get you out of a pickle

--- GraphAlgs.cpp
+++ GraphAlgs.cpp
@@ -8,8 +8,10 @@
 
 #include "GraphAlgs.h"
 #include <iostream>
-std::pair<std::vector<NodeID>, EdgeWeight> tour(int* arr, int arr_len, int start, EdgeWeight cur,
-                                                std::pair<std::vector<NodeID>, EdgeWeight> best, Graph* g);
+
+using namespace std;
+std::pair< std::vector<NodeID>, EdgeWeight > best;
+void tour(int* arr, int arr_len, int start, Graph* g);
 
 std::pair<std::vector<NodeID>, EdgeWeight> TSP(Graph* G){
     int arr_len = G->size();
@@ -24,14 +26,19 @@ std::pair<std::vector<NodeID>, EdgeWeight> TSP(Graph* G){
             bsf += G->weight(i, i-1);
     }
     bsf+=G->weight(arr_len-1, 0);
-    return tour(arr, arr_len, 0, 0.0, std::make_pair(ids, bsf), G);
+    best = std::make_pair(ids, bsf);
+    for(int i = 0; i<arr_len; i++)
+        cout<<best.first[i]<< endl;
+    tour(arr, arr_len, 0, G);
+    for(int i = 0; i<arr_len; i++)
+        cout<<best.first[i]<< endl;
+
+    return best;
 }
 
-std::pair<std::vector<NodeID>, EdgeWeight> tour(int* arr, int arr_len, int start, EdgeWeight cur,
-                                                std::pair<std::vector<NodeID>, EdgeWeight> best, Graph* g){
-    std::pair<std::vector<NodeID>, EdgeWeight> ret;
-    if(arr_len - start == 1){
-        EdgeWeight final = 0/*cur + g->weight(arr[0], arr[arr_len-1])*/;
+void tour(int* arr, int arr_len, int start, Graph* g){
+    /* if(arr_len - start == 1){
+        EdgeWeight final = 0;
         for(int i = 1; i<arr_len; i++)
             final+=g->weight(arr[i], arr[i-1]);
         final += g->weight(arr[0], arr[arr_len-1]);
@@ -41,19 +48,35 @@ std::pair<std::vector<NodeID>, EdgeWeight> tour(int* arr, int arr_len, int start
             temp.resize(arr_len);
             for(int i = 0; i<arr_len; i++)
                 temp[i] = arr[i];
-            
-            ret = std::make_pair(temp, final);
+            best = std::make_pair(temp, final);
         }
         
     } else {
         for(int i = start; i<arr_len; i++){
-            int temp = arr[start];
-            arr[start] = arr[i];
-            arr[i] = arr[start];
-            ret = tour(arr, arr_len, start+1, cur/*+g->weight(arr[i], arr[i-1])*/, best, g);
-            arr[i] = arr[start];
-            arr[start] = temp;
+            arr[start] += arr[i];
+            arr[i] = arr[start] - arr[i];
+            arr[start] -= arr[i];
+            
+            tour(arr, arr_len, start+1, g);
+            
+            arr[start] += arr[i];
+            arr[i] = arr[start] - arr[i];
+            arr[start] -= arr[i];
         }
-    }
-    return ret;
+    } */
+    
+    do {
+        EdgeWeight final = 0;
+        for(int i = 1; i<arr_len; i++)
+            final+=g->weight(arr[i], arr[i-1]);
+        final += g->weight(arr[0], arr[arr_len-1]);
+        //Check to see if this is the best of the best of the best tour. <- MIB reference
+        if(final < best.second){
+            std::vector<NodeID> temp;
+            temp.resize(arr_len);
+            for(int i = 0; i<arr_len; i++)
+                temp[i] = arr[i];
+            best = std::make_pair(temp, final);
+        }
+    } while(next_permutation(arr, arr+arr_len));
 }
